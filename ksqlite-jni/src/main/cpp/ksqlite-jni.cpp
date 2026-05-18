b5e0814b153484281f66bf3ecf19fcc32ba952eb
Maanrifa <dev.manrif@gmail.com>
2026-05-19 00:25:23 +0200

    WIP

--- ksqlite-jni/src/main/cpp/ksqlite-jni.cpp
+++ ksqlite-jni/src/main/cpp/ksqlite-jni.cpp
@@ -15,10 +15,6 @@
 // Classes
 ///////////////////////////////////////////////////////////////////////////
 
-// Java
-#define JAVA_STRING "java.lang.String"
-
-// Ksqlite
 #define KSQLITE_JNI_EXCEPTION "KsqliteJniException"
 #define DESTRUCTOR_CALLBACK "DestructorCallback"
 #define AUTO_EXTENSION_CALLBACK "AutoExtensionCallback"
@@ -54,7 +50,7 @@ static inline jobject requireNonNull(
     return object;
 }
 
-#define RequireNull(O) requireNonNull(env, (O))
+#define RequireNull(O) requireNull(env, (O))
 #define RequireNonNull(O) requireNonNull(env, (O))
 
 /**
@@ -84,6 +80,31 @@ static inline void raiseOutOfMemory(JNIEnv* const env) {
  * Raises a fatal error if expression E is false.
  */
 #define OutOfMemoryCheck(E) if (!(E)) raiseOutOfMemory(env)
+
+/**
+ * Returns true if an exception has been reported.
+ */
+static inline bool reportException(
+    JNIEnv* const env,
+    bool clear
+) {
+    if (env->ExceptionCheck()) {
+        env->ExceptionDescribe();
+
+        if (clear) {
+            env->ExceptionClear();
+        }
+
+        return true;
+    }
+
+    return false;
+}
+
+#define IfExceptionThrownClear(clear) if (reportException(env, clear))
+#define IfExceptionThrown IfExceptionThrownClear(false)
+#define IfExceptionNotThrownClear(clear) if (!reportException(env, clear))
+#define IfExceptionNotThrown IfExceptionNotThrownClear(false)
 
 ///////////////////////////////////////////////////////////////////////////
 // JNI reference management helpers
@@ -185,31 +206,6 @@ static inline jmethodID getMethodIdOrDie(
 #define RequireKsqliteMethod(O, name, signature, className) \
     RequireKsqliteClassMethod(O.klass, name, signature, className)
 
-/**
- * Returns true if an exception has been reported.
- */
-static inline bool reportException(
-    JNIEnv* const env,
-    bool clear
-) {
-    if (env->ExceptionCheck()) {
-        env->ExceptionDescribe();
-
-        if (clear) {
-            env->ExceptionClear();
-        }
-
-        return true;
-    }
-
-    return false;
-}
-
-#define IfExceptionThrownClear(clear) if (reportException(env, clear))
-#define IfExceptionThrown IfExceptionThrownClear(false)
-#define IfExceptionNotThrownClear(clear) if (!reportException(env, clear))
-#define IfExceptionNotThrown IfExceptionNotThrownClear(false)
-
 ///////////////////////////////////////////////////////////////////////////
 // Mutex
 ///////////////////////////////////////////////////////////////////////////
@@ -291,18 +287,6 @@ struct Class {
 static struct {
     JavaVM* jvm;
 
-    // Java classes
-    struct {
-        struct : Class {
-            jmethodID constructor; // ([BLjava/nio/charset/Charset;)V
-            jmethodID getBytes; // (Ljava/nio/charset/Charset;)[B
-        } string;
-
-        struct {
-            jobject utf8; // Ljava/nio/charset/Charset;
-        } charset;
-    } java;
-
     // KsqliteJni classes
     struct {
         struct : Class {
@@ -323,38 +307,10 @@ static struct {
 } KsqliteJniGlobalState;
 
 #define KS KsqliteJniGlobalState
-#define KJS KS.java.string
-#define KJC KS.java.charset
 #define KKJE KS.ksqlite.jniException
 #define KKDC KS.ksqlite.destructorCallback
 #define KHAE KS.handlers.autoExtension
 #define KHAP KS.handlers.autoVacuumPages
-
-/**
- * Initializes and caches the Java related classes and objects.
- */
-static void initializeJavaClassCache(JNIEnv* env) {
-    KJS.klass = RequireClass("java/lang/String");
-    KJS.constructor = RequireMethod(KJS, "<init>", "([BLjava/nio/charset/Charset;)V", JAVA_STRING);
-    KJS.getBytes = RequireMethod(KJS, "getBytes", "(Ljava/nio/charset/Charset;)[B", JAVA_STRING);
-
-    const auto klass = env->FindClass("java/nio/charset/StandardCharsets");
-    ExceptionClearAndAbort("Error getting reference to java.nio.charset.StandardCharsets class");
-
-    const auto fieldId = env->GetStaticFieldID(klass, "UTF_8", "Ljava/nio/charset/Charset;");
-
-    ExceptionClearAndAbort(
-        "Error getting reference to java.nio.charset.StandardCharsets.UTF_8 field"
-    );
-
-    KJC.utf8 = GlobalRefCreate(env->GetStaticObjectField(klass, fieldId));
-
-    ExceptionClearAndAbort(
-        "Error getting reference to java.nio.charset.StandardCharsets.UTF_8 instance"
-    );
-
-    LocalRefDestroy(klass);
-}
 
 /**
  * Initializes and caches the Ksqlite related classes and objects.
@@ -416,7 +372,6 @@ JNI_OnLoad(
     const auto env = retrieveJniEnv();
 
     // Classes
-    initializeJavaClassCache(env);
     initializeKsqliteJniCache(env);
 
     // Sqlite3
@@ -433,11 +388,6 @@ JNI_OnUnload(
     void* reserved
 ) {
     const auto env = retrieveJniEnv();
-
-    GlobalRefDestroy(KJS.klass);
-    KJS.constructor = nullptr;
-
-    GlobalRefCreate(KJC.utf8);
 
     GlobalRefDestroy(KKDC.klass);
     KKDC.destroy = nullptr;
@@ -584,138 +534,109 @@ static jbyteArray createByteArray(
 // String helpers
 ///////////////////////////////////////////////////////////////////////////
 
-/**
- * Compute a string length that is limited to what can be stored in lower 30 bits of a 32-bit signed
- * integer.
+/*
+ * Converts Java String (UTF-16) to UTF-8 C string using Android AOSP Unicode transcoder.
+ * Output must be freed with sqlite3_free().
  *
- * The value returned will never be negative.  Nor will it ever be greater than the actual length of
- * the string.  For very long strings (greater than 1GiB) the value returned might be less than the
- * true string length.
- *
- * From official <sqlite3.c>
+ * Returns nullptr on error or null input.
  */
-static int sqlite3Strlen30(const char* const z) {
-    if (z == nullptr) {
-        return 0;
-    }
-
-    return 0x3fffffff & (int) strlen(z);
-}
-
-/**
- * Uses the java.lang.String(byte[],Charset) constructor to create a new String from UTF-8 string
- * buffer. size is the number of bytes to copy. If size<0 then sqlite3Strlen30() is used to
- * calculate it.
- *
- * Returns NULL if buffer is NULL or on OOM, else returns a new jstring owned by the caller.
- *
- * Sidebar: this is a painfully inefficient way to convert from standard UTF-8 to a Java string, but
- * JNI offers only algorithms for working with MUTF-8, not UTF-8.
- *
- * From <sqlite3_jni.c>
- */
-static jstring utf8ToJstring(
-    JNIEnv* const env,
-    const char* const buffer,
-    int size
+static char* jstringToUtf8(
+    JNIEnv* env,
+    jstring string,
+    size_t* outLength
 ) {
-    if (buffer == nullptr) {
+    if (string == nullptr) {
+        if (outLength != nullptr) {
+            *outLength = 0;
+        }
+
         return nullptr;
     }
 
-    jstring string = nullptr;
+    const auto chars = env->GetStringChars(string, nullptr);
 
-    if (0 == size || (size < 0 && !buffer[0])) {
-        /* Fast-track the empty-string case via the MUTF-8 API. We could hypothetically do this for
-         * any strings where size<4 and buffer is  NUL-terminated and none of buffer[0..3] are NUL
-         * bytes. */
-        string = env->NewStringUTF("");
-        OutOfMemoryCheck(string != nullptr);
-    } else {
-        jbyteArray byteArray;
-
-        if (size < 0) {
-            size = sqlite3Strlen30(buffer);
-        }
-
-        byteArray = CreateByteArray(buffer, size);
-
-        if (byteArray) {
-            string = reinterpret_cast<jstring>(
-                env->NewObject(KJS.klass, KJS.constructor, byteArray, KJC.utf8)
-            );
-
-            IfExceptionThrown {
-                env->ExceptionDescribe();
-                env->ExceptionClear();
-            }
-
-            LocalRefDestroy(byteArray);
-        }
-
-        OutOfMemoryCheck(string);
+    if (chars == nullptr) {
+        return nullptr;
     }
+
+    const auto length = env->GetStringLength(string);
+
+    const auto utf8Length = utf16_to_utf8_length(
+        reinterpret_cast<const char16_t*>(chars),
+        length
+    );
+
+    if (utf8Length <= 0) {
+        env->ReleaseStringChars(string, chars);
+        return nullptr;
+    }
+
+    const auto utf8 = static_cast<char*>(sqlite3_malloc(utf8Length + 1));
+    OutOfMemoryCheck(utf8);
+
+    utf16_to_utf8(
+        reinterpret_cast<const char16_t*>(chars),
+        length,
+        utf8,
+        utf8Length + 1
+    );
+
+    env->ReleaseStringChars(string, chars);
+
+    if (outLength != nullptr) {
+        *outLength = utf8Length;
+    }
+
+    return utf8;
+}
+
+/*
+ * Converts UTF-8 C string to Java String (UTF-16) using Android AOSP Unicode transcoder.
+ */
+static jstring utf8ToJstring(
+    JNIEnv* env,
+    const char* utf8,
+    size_t length
+) {
+    if (utf8 == nullptr) {
+        return nullptr;
+    }
+
+    if (length == -1) {
+        // This is what is used by sqlite internally
+        length = 0x3fffffff & static_cast<int>(strlen(utf8));
+    }
+
+    const auto utf16Length = utf8_to_utf16_length(
+        reinterpret_cast<const uint8_t*>(utf8),
+        length
+    );
+
+    if (utf16Length <= 0) {
+        return env->NewString(nullptr, 0);
+    }
+
+    const auto utf16 = static_cast<jchar *>(sqlite3_malloc((utf16Length + 1u) * sizeof(jchar)));
+
+    if (utf16 == nullptr) {
+        return nullptr;
+    }
+
+    utf8_to_utf16(
+        reinterpret_cast<const uint8_t*>(utf8),
+        length,
+        reinterpret_cast<char16_t*>(utf16),
+        utf16Length + 1
+    );
+
+    jstring string = env->NewString(utf16, utf16Length);
+    sqlite3_free(utf16);
 
     return string;
 }
 
-/**
-static char * utf8ToBuffer() {
-
-}
-
-/**
- * Allocates a null terminated buffer to hold a string.
- *
- * Note that Java exception must be checked after the function return if length is greater than
- * byteArray actual length.
- */
-static char* byteArrayToBuffer(
-    JNIEnv* const env,
-    jbyteArray byteArray,
-    jsize length
-) {
-    if (byteArray == nullptr) {
-        return nullptr;
-    }
-
-    auto bytes = (char*) sqlite3_malloc(length);
-
-    if (bytes != nullptr) {
-        env->GetByteArrayRegion(byteArray, 0, length, (jbyte*) bytes);
-
-        IfExceptionNotThrown {
-            bytes[length] = 0;
-        }
-    }
-
-    return bytes;
-}
-
-/**
- * Allocates a null terminated buffer to hold a string and write the length to outLength.
- */
-static char* byteArrayToBufferOut(
-    JNIEnv* const env,
-    jbyteArray byteArray,
-    jsize* outLength
-) {
-    if (byteArray != nullptr) {
-        return nullptr;
-    }
-
-    const auto length = env->GetArrayLength(byteArray);
-
-    if (outLength != nullptr) {
-        *outLength = length;
-    }
-
-    return byteArrayToBuffer(env, byteArray, length);
-}
-
-#define BufferToUtf8(buffer, size) bufferToUtf8(env, buffer, size)
-#define ByteArrayToBuffer(byteArray, length) byteArrayToBuffer(env, byteArray, lenght)
-#define ByteArrayToBufferOut(byteArray, outLength) byteArrayToBufferOut(env, byteArray, outLength)
+#define JstringToUtf8(string, outLength) jstringToUtf8(env, string, outLength)
+#define Utf8ToJstring(utf8, length) utf8ToJstring(env, utf8, length)
 
 ///////////////////////////////////////////////////////////////////////////
 // Callbacks
@@ -782,10 +703,10 @@ static int callAutoExtensionCallback(
         IfExceptionThrown {
             rc = SQLITE_ERROR;
         } else {
-            const auto buffer = ByteArrayToBufferOut(message, nullptr);
+            const auto utf8 = JstringToUtf8(message, nullptr);
 
-            if (buffer != nullptr) {
-                *pzErr = sqlite3_mprintf(buffer);
+            if (utf8 != nullptr) {
+                *pzErr = sqlite3_mprintf(utf8);
                 sqlite3_free(message);
             }
 
@@ -815,7 +736,7 @@ static unsigned int callAutoVacuumPagesCallback(
     const auto env = retrieveJniEnv();
     HandlerCallbackConsume(KHAP);
 
-    const auto schema = BufferToUtf8(zSchema, -1);
+    const auto schema = Utf8ToJstring(zSchema, -1);
 
     auto result = env->CallIntMethod(
         callback,
